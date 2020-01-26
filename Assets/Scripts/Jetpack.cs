3625020c86c23e771cc421224485d7a2028b0213
vladstheawesome <vmabhena@gmail.com>
2020-01-26 00:49:37 +0000

    Adding audio on Thrusting

--- Assets/Scripts/Jetpack.cs
+++ Assets/Scripts/Jetpack.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Jetpack : MonoBehaviour
 {
     Rigidbody rigidbody;
+    AudioSource jetpacksound;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        jetpacksound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,15 +26,23 @@ public class Jetpack : MonoBehaviour
         if(Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!jetpacksound.isPlaying)
+            {
+                jetpacksound.Play();
+            }
+        }
+        else
+        {
+            jetpacksound.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
