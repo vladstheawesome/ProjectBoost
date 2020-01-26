ba946a418dae237e2013fef66cf0dc467ee1f734
vladstheawesome <vmabhena@gmail.com>
2020-01-26 22:40:28 +0000

    Resolving Movement and Serializable Field Levers

--- Assets/Scripts/Jetpack.cs
+++ Assets/Scripts/Jetpack.cs
@@ -5,44 +5,56 @@ using UnityEngine;
 
 public class Jetpack : MonoBehaviour
 {
-    Rigidbody rigidbody;
-    AudioSource jetpacksound;
+    Rigidbody rigidBody;
+    AudioSource jetpackSound;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
-        jetpacksound = GetComponent<AudioSource>();
+        rigidBody = GetComponent<Rigidbody>();
+        jetpackSound = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
-        if(Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        //float thisThrust = mainThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            rigidbody.AddRelativeForce(Vector3.up);
-            if (!jetpacksound.isPlaying)
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!jetpackSound.isPlaying)
             {
-                jetpacksound.Play();
+                jetpackSound.Play();
             }
         }
         else
         {
-            jetpacksound.Stop();
+            jetpackSound.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
-    }
+
+        rigidBody.freezeRotation = false; // resume physics
+    }    
 }
